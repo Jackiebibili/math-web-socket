266ddac9ad6fca8fc1ea5efcb7ee53f34c30ef68
Jack Li <jack.li@devz.ai>
2022-04-26 01:53:45 -0500

    Add user packet classes and implement the client and server functions

---
+++ src/user_dup_packet.py
@@ -0,0 +1,7 @@
+from user_msg_packet import UserMsgPacket
+from src.packet import PacketType
+
+class UserDupPacket(UserMsgPacket):
+   def __init__(self, username, msg):
+      super().__init__(username, msg)
+      self.type = PacketType.INVALID
