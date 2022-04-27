8131ab59f68704aa09dc5da2d8e16c5a351b5644
Jessica Dara <jkathleendara@gmail.com>
2022-04-26 20:55:33 -0500

    Logging

--- src/client.py
+++ src/client.py
@@ -1,29 +1,70 @@
 import socket
 from threading import Thread
+from threading import Thread, Semaphore
 from queue import Queue
+from user_join_packet import UserJoinPacket
+from user_msg_packet import UserMsgPacket
+from user_terminate_packet import UserTerminatePacket
+from src.packet import Packet, PacketType
+
+DEFAULT_BUFFER_SIZE = 4096 # 4k
 
 class Client():
    def __init__(self):
       # tasks = ['tasks']
+   def __init__(self, username, buf_size=DEFAULT_BUFFER_SIZE):
       self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.client_socket.connect(('localhost', 8080))
       self.usr = "testing"
+      self.usr = username
+      self.buf_size = buf_size
       self.q = Queue()
       # self.q.put(self.usr)
+      self.conn_est = Semaphore(0)
 
    def receive_msg(self):
+      pkt_handler = Packet()
       while True:
          data = self.client_socket.recv(4096)
          msg = data.decode('utf-8')
          print(f"client receiving {msg} from server\n")
+         data = self.client_socket.recv(self.buf_size)
+         for pkt in pkt_handler.decode(data):
+            if pkt.type == PacketType.USER_JOIN:
+               # ack joining
+               self.conn_est.release()
+            elif pkt.type == PacketType.USER_TERMINATE:
+               # ack terminating
+               self.conn_est.acquire()
+            elif pkt.type == PacketType.USER_MESSAGE:
+               # normal packet
+               result = pkt.message.decode('utf-8')
+               # print(result + '\n')
+            elif pkt.type == PacketType.INVALID:
+               # invalid packet - discard
+               print("Packet received is invalid and discarded\n")
 
    def send_msg(self):
       msg = input('enter your message: ')
       self.q.put(msg)
+   def send_msg(self, msg):
+      self.q.put(UserMsgPacket(self.usr, msg))
       while not self.q.empty():
          item = self.q.get()
          self.client_socket.send(bytearray(item.encode()))
          print(f"client sent {item} to server\n")
+
+         self.client_socket.send(item.encode())
+         # print(f"client sent {item} to server\n")
+
+   def send_math_expressions(self, arr):
+       # handshaking packet
+      self.q.put(UserJoinPacket(self.usr))
+      self.conn_est.acquire()
+      for msg in arr:
+         self.send_msg(msg)
+      # closing packet
+      self.q.put(UserTerminatePacket(self.usr))
 
 
 
@@ -33,3 +74,4 @@ if __name__ == "__main__":
    Thread(target=client.send_msg).start()
    Thread(target=client.receive_msg).start()
    pass
+   
--- src/server.py
+++ src/server.py
@@ -1,19 +1,88 @@
+import time
+from datetime import datetime
+from queue import Queue
 from socketserver import BaseRequestHandler, ThreadingTCPServer
 from threading import Thread
+from threading import Thread, Semaphore
+from src.packet import Packet, PacketType
+from user_msg_packet import UserMsgPacket
+from user_dup_packet import UserDupPacket
 
+log_queue = Queue()
+
+def _log(msg):
+	log_queue.put(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {msg}')
+
+DEFAULT_BUFFER_SIZE = 4096  # 4k
 PORT = 8080
 
 class TCPHandler(BaseRequestHandler):
    def handle(self):
       sock = self.request
       client_name = sock.getpeername()
+
+def configTCPHandler(buf_size=DEFAULT_BUFFER_SIZE):
+   pkt_handler = Packet()
+   sessions = dict()
+   forward_queue = Queue()
+   forward_sem = Semaphore(0)
+
+   def message_forwarder():
       while True:
          # server receives info
          data = sock.recv(4096)
          msg = data.decode('utf-8')
-         print(f"server receiving {msg} from {client_name}\n")
+         _log(f'Server is receiving {msg} from {client_name}.')
          # server sends info back to client
          sock.send(bytearray(msg.upper().encode()))
+         if forward_queue.empty():
+            forward_sem.acquire()
+         client, socket, pkt = forward_queue.get()
+         if client in sessions:
+            socket.send(pkt.encode())
+            _log(f'Client is receiving {pkt.type} from Server.')
+
+
+   class TCPHandler(BaseRequestHandler):
+      def handle(self):
+         sock = self.request
+         start = time.time()
+         client_name = sock.getpeername()
+         while True:
+            # server receives info
+            data = sock.recv(buf_size)
+            for pkt in pkt_handler.decode(data):
+               if pkt.type == PacketType.USER_JOIN:
+                  if client_name in sessions:
+                     forward_queue.put((client_name, sock, UserDupPacket('server', 'dup'.encode('utf-8'))))
+                  else:
+                     _log(f'New connection from {client_name}')
+                     sessions[client_name] = (sock, start)
+                     forward_queue.put((client_name, sock, pkt))
+               elif pkt.type == PacketType.USER_TERMINATE:
+                  if client_name not in sessions:
+                     forward_queue.put(
+                        (client_name, sock, UserDupPacket('server', 'dup'.encode('utf-8'))))
+                  else:
+                     end = time.time()
+                     time = (end - sessions[client_name][1])
+                     period = str(datetime.timedelta(minutes=time))
+                     _log(f'Terminating connection from {client_name} that lasted {period} minutes')
+                     del sessions[client_name]
+                     forward_queue.put((client_name, sock, pkt))
+               elif pkt.type == PacketType.USER_MESSAGE:
+                  _log(f'Server is receiving {ReceiveExpression} from {client_name}')
+                  # Todo: math expr evaluation
+                  ans = 0.0
+                  result = UserMsgPacket('server', str(ans).encode('utf-8'))
+                  forward_queue.put((client_name, sock, result))
+               forward_sem.release()
+            # print(f"server receiving {msg} from {client_name}\n")
+            # server sends info back to client
+            # sock.send(bytearray(msg.upper().encode()))
+   return TCPHandler, message_forwarder
+   
+
 
 class Server(ThreadingTCPServer):
    pass
@@ -21,4 +90,4 @@ class Server(ThreadingTCPServer):
 if __name__ == "__main__":
    server = Server(('localhost', PORT), TCPHandler)
    print(f"=== server serving on port {PORT} ===\n")
-   Thread(target=server.serve_forever).start()
+   Thread(target=server.serve_forever).start() 
